18b23738f93eb1196c38a85a93b1c4cd2c90cdec
‘RodrigoDuarte655’ <rodrigodsluz@gmail.com>
2020-02-12 11:26:52 -0300

    new

---
+++ exercises/a_first.py
@@ -0,0 +1,19 @@
+
+
+'''
+   
+
+'''
+
+'''
+A = int(input(''))
+B = int(input(''))
+
+X = (A + B)
+
+print('X = ' + str(X))
+print('X = {X}'.format(X = X))
+print(f'X = {X}')
+
+print(type(X))
+'''
