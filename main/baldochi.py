ae2271c2fd0ea005e8182d2bb48d7b1535fa234a
rodrigodsluz <rodrigodsluz@gmail.com>
2020-04-19 15:36:49 -0300

    new

--- main/baldochi.py
+++ main/baldochi.py
@@ -1,4 +1,4 @@
-""" Escreva uma função que receba um float representando o
+""" AEscreva uma função que receba um float representando o
 valor da temperatura em Celsius e retorne a temperatura
 equivalente em Farenheit. Em seguida, escreva um código
 que leia uma temperatura em Celsius e informe o valor
